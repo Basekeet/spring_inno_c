163a18f3ab14b6ba0d8dc7ce4f04ee207bc33c84
rmalyushev_1 <rmalyushev_1@miem.hse.ru>
2023-02-12 18:35:00 +0300

    file_input_output

---
+++ 1 занятие/file_input_output.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main (){
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+
+    int n;
+    fin >> n;
+
+    fout << n + 2; 
+}
